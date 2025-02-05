c59a3b9a4ebabbc898091863c8c1626f8629c893
Diego Alejandro Torres Paramo <torresparamodiegoalejandro@gmail.com>
2025-02-05 12:43:08 -0600

    agregar menu al proyecto

--- src/main/java/com/example/tap2025/HelloApplication.java
+++ src/main/java/com/example/tap2025/HelloApplication.java
@@ -1,23 +1,59 @@
 package com.example.tap2025;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
+    private VBox vBox;
+    private MenuBar mnbPrincipal;
+    private Menu menCompetencia1,menCompetencia2;
+    private MenuItem mitCalculadora;
+    private Scene escena;
+    void creaUI(){
+        mitCalculadora = new MenuItem("Calculadora");
+        menCompetencia1 = new Menu("Competencia 1");
+        menCompetencia1.getItems().addAll(mitCalculadora);
+        mnbPrincipal=new MenuBar();
+        mnbPrincipal.getMenus().addAll(menCompetencia1);
+        vBox = new VBox();
+    }
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+
+        stage.setTitle("Hola Mundo de Eventos :)");
+        stage.setScene(new Scene(vBox));
         stage.show();
+        stage.setMaximized(true);
     }
 
     public static void main(String[] args) {
         launch();
     }
 }
+/*
+        btnSaludo=new Button("Hola amigo :)");
+        btnSaludo.setOnAction(event -> clickEvent());
+        btnSaludo2=new Button("Hola amiga :)");
+        btnSaludo2.setOnAction(event ->clickEvent());
+        btnSaludo3=new Button("Hola amige :)");
+        vBox=new VBox(btnSaludo,btnSaludo2,btnSaludo3);
+        vBox.setSpacing(15);
+        vBox.setPadding(new Insets(10,0,0,0));
+        stage.setTitle("Hola Mundo de Eventos :)");
+        stage.setScene(new Scene(vBox,200,200));
+        stage.show();
+        stage.setMaximized(true);
+
+
+*/
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.tap2025 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.tap2025 to javafx.fxml;
