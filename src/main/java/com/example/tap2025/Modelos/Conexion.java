ce43594d3d102676e45403237b7d8fe76135f7e2
Andres L <gitlab.tr1pj@slmail.me>
2025-06-03 03:06:44 -0600

    test ignore

--- src/main/java/com/example/tap2025/Modelos/Conexion.java
+++ src/main/java/com/example/tap2025/Modelos/Conexion.java
@@ -6,16 +6,16 @@ import java.sql.DriverManager;
 public class Conexion {
     static private String DB = "restaurante";
     static private String USER = "admin";
-    static private String PWD = "1234";
+    static private String PWD = "123";
     static private String HOST = "localhost";
     static private String PORT = "3306";
     public static Connection connection;
 
     public static void createConnection(){
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");//etiqueta para generar conexion, javaMore actions
-            connection=DriverManager.getConnection("jdbc:mysql://"+HOST+":"+PORT+"/"+DB,USER,PWD);//se abre un socket, conexion a host
-            System.out.println("Conexion establecida °3°");
+            Class.forName("org.mariadb.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mariadb://" + HOST + ":" + PORT + "/" + DB, USER, PWD);
+            System.out.println("Conexión establecida a la Base de Datos :)");
         }catch (Exception e){
             e.printStackTrace();
         }
